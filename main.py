1bf573596d150d99a34db98da108e8fbf5cb02ff
obrunofilipe <brunofilipe377@gmail.com>
2022-05-11 00:37:40 +0100

    yacc

--- main.py
+++ main.py
@@ -18,14 +18,18 @@ def listToString(list):
         i += 1
     return ret
 
-literals = {'[',']',','}
+literals = {'[',']',',','='}
 
-tokens = ["LEXER","LITERALS","IGNORE","TOKENS","EXPREG","STRING","COMMENT","NEWLINE","RETURN","ERROR","TOKEN","CODIGO", "ATRIB"]
+tokens = ["LEXER","YACC","LITERALS","IGNORE","TOKENS","EXPREG","STRING","COMMENT","NEWLINE",
+          "RETURN","ERROR","TOKEN","ATRIB","PRECEDENCE","TUPLO","ID","VALOR","REGRAGRAM","CODIGO",
+          "GRAMMAR", "ENDGRAMMAR","RESTO"]
 
 states = [
     ("lexer", "exclusive"),
     ("tokens", "exclusive"),
-    ("regex", "exclusive")
+    ("regex", "exclusive"),
+    ("yacc", "exclusive"),
+    ("grammar", "exclusive")
 ]
 
 
@@ -46,13 +50,23 @@ def t_tokens_NEWLINE(t):
 
 #regex
 
-def t_regex_CODIGO(t):
-    r'[^\n]+'
+def t_regex_RETURN(t):
+    r'return[^\n]+'
     lexer.pop_state()
     return t
 
+def t_regex_ERROR(t):
+    r'error[^\n]+'
+    lexer.pop_state()
+    return t
 
 #lexer
+
+def t_lexer_YACC(t):
+    r'YACC'
+    lexer.pop_state()
+    lexer.push_state("yacc")
+    return t
 
 def t_lexer_ATRIB(t):
     r'='
@@ -76,13 +90,55 @@ def t_lexer_TOKENS(t):
     return t
 
 def t_lexer_COMMENT(t):
-    "\#\#[^\n]+$"
+    r"\#\#[^\n]+$"
     return t
 
 def t_lexer_EXPREG(t):
     r'(\\\s|\S)+'
     lexer.push_state("regex")
     return t
+
+
+#grammar state
+
+def t_grammar_REGRAGRAM(t):
+    r'[a-zA-Z]+\s:\s[^{]+'
+    return t
+
+def t_grammar_CODIGO(t):
+    r"\{.*\}"
+    return t
+
+def t_grammar_ENDGRAMMAR(t):
+    r"%%"
+    lexer.pop_state()
+    lexer.pop_state()
+
+#yacc
+
+
+
+def t_yacc_PRECEDENCE(t):
+    r"%precedence"
+    return t
+
+def t_yacc_TUPLO(t):
+    r"\(('right'|'left'|'nonassoc'),(\'(\w+|[^A-Za-z,'])\'(,)?)+\)"
+    return t
+
+def t_yacc_GRAMMAR(t):
+    r"%grammar"
+    lexer.push_state("grammar")
+    return t
+
+def t_yacc_ID(t):
+    r"[a-zA-Z]+"
+    return t
+
+def t_yacc_VALOR(t):
+    r"[^\n=]+"
+    return t
+
 
 #INITIAL
 
@@ -91,23 +147,42 @@ def t_LEXER(t):
     lexer.push_state("lexer")
     return t
 
+def t_RESTO(t):
+    r'(.|\n)+'
+    return t
+
+
 #ignore
 t_tokens_ignore = " \t\r"
-t_regex_ignore = " \n\t\r"
+t_regex_ignore = " \t\r"
 t_lexer_ignore = " \n\t\r"
+t_yacc_ignore = " \n\t\r"
+t_grammar_ignore = " \n\t\r"
 t_ignore = " \n\t\r"
 
 #error
 
 def t_ANY_error(t):
-    print("Illegal character!", t.value[0])
+    print("Illegal character!", t.value)
 
+f = open("test.txt")
+
+content = f.read()
 
 lexer = lex.lex()
 
+lexer.input(content)
+for tok in lexer:
+    print(tok)
+
+
+
+#gramática ficheiro
 
 def p_ficheiro(p):
-    "ficheiro : lex"
+    "ficheiro : lex yacc"
+
+#gramática lex
 
 def p_lex(p):
     "lex : LEXER conteudolex"
@@ -145,7 +220,6 @@ def p_tokens_token(p):
 def p_tokens_lista(p):
     "tokens : TOKEN ',' tokens"
 
-
 def p_regras_vazio(p):
     "regras : "
     
@@ -155,9 +229,47 @@ def p_regras_lista(p):
 def p_regra(p):
     "regra : EXPREG acao"
 
+def p_acao_return(p):
+    "acao : RETURN"
 
-def p_acao(p):
-    "acao : CODIGO"
+def p_acao_error(p):
+    "acao : ERROR"
+
+#gramática yacc
+
+def p_yacc(p):
+    "yacc : YACC coteudoyacc"
+
+def p_conteudoyacc(p):
+    "conteudoyacc : precedence variables producoes"
+
+def p_precedence(p):
+    "precedence : PRECEDENCE '=' '[' tuplos ']'"
+
+def p_tuplos_vazio(p):
+    "tuplos : "
+
+def p_tuplos_varios(p):
+    "tuplos : tuplos ',' TUPLO"
+
+def p_variables_vazio(p):
+    "variables : "
+
+def p_variables_varias(p):
+    "variables : variables variable"
+
+def p_variable(p):
+    "variable : ID '=' VALOR"
+
+def p_producoes_vazio(p):
+    "producoes : "
+
+def p_producoes_varias(p):
+    "producoes : producoes producao"
+    
+def p_producao(p):
+    "producao : REGRAGRAM CODIGO"
+
 
 
 def p_error(p):
@@ -170,4 +282,4 @@ f = open("test.txt")
 
 content = f.read()
 
-parser.parse(content)
+#parser.parse(content)
